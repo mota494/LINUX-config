13f16bb0136912e63a2e33219eba7f1d215893cf
Miguel Loureiro Mota <mloureir@c1r2s4.42porto.com>
2024-03-20 12:07:28 +0000

    added install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
+#Deletes old files and creates new folders for necessary files
+
+rm -r $HOME/bin
+mkdir $HOME/bin
+rm $HOME/.config/starship.toml
+rm -r $HOME/.config/nvim
+rm $HOME/.zshrc
+
+#Moves the contents to the correct folder and installs all the appimages necessary
+
 mv nvim $HOME/.config
 mv starship.toml $HOME/.config
 mv .zshrc $HOME
-mkdir $HOME/bin
+cd $HOME/bin
+wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+chmod +777 nvim.appimage
+wget https://github.com/Mastermindzh/tidal-hifi/releases/download/5.9.0/tidal-hifi-5.9.0.AppImage
+chmod +777 tidal-hifi-5.9.0.AppImage
+wget https://github.com/OpenShot/openshot-qt/releases/download/v3.1.1/OpenShot-v3.1.1-x86_64.AppImage
+chmod +777 OpenShot-v3.1.1-x86_64.AppImage
