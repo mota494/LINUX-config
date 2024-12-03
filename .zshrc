bd4089e4d609ddacd9248647d2556cb213ea09e9
Mota494 <migueloureiromota@gmail.com>
2024-12-03 09:21:55 +0000

    new install script, more nvim extensions (very usefull)

--- .zshrc
+++ .zshrc
@@ -6,14 +6,11 @@ setopt auto_cd
 #alias
 
 alias vconf="vim $HOME/.config/nvim"
-alias tconf="vim $HOME/.tmux.conf"
-alias tidal="$HOME/bin/tidal-hifi-5.9.0.AppImage"
+alias tidal="$HOME/bin/Tidal"
 alias vim="$HOME/bin/nvim.appimage"
-alias zen="$HOME/sgoinfre/zen.linux-specific/zen/zen"
-alias obsidian="$HOME/bin/Obsidian-1.7.4.AppImage"
-alias normer="norminette | grep -F Error"
-alias normnum="norminette | grep "Error" | wc -l" 
+alias obsidian="$HOME/bin/Obsidian"
 alias kitty="sh $HOME/bin/open_kitty.sh"
+
 #path_config
 
 path=(
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #Deletes old files and creates new folders for necessary files
 
+echo "===========================// λ Installation has started λ //==========================="
+
 rm -r $HOME/bin
 mkdir $HOME/bin
 rm $HOME/.config/starship.toml
@@ -7,9 +9,16 @@ rm -r $HOME/.config/nvim
 
 #Moves the contents to the correct folder and installs all the appimages necessary
 
-mv open_kitty.sh $HOME/bin
 mv nvim $HOME/.config
+mv open_kitty.sh $HOME/bin
 mv starship.toml $HOME/.config
+
+#Installs kitty terminal and starshi[
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+curl -sS https://starship.rs/install.sh | sh -s -- -b $HOME/bin
+
+#Checks if all the folders were created succefully and send all the files to them, if there's a folder that's needed and it doesn't exist it gets created
 
 if [ -f $HOME/.zshrc ]; then
 	cp $HOME/.zshrc $HOME/.oldzshrc
@@ -17,32 +26,6 @@ if [ -f $HOME/.zshrc ]; then
 else
 	mv .zshrc $HOME
 fi
-
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-curl -sS https://starship.rs/install.sh | sh -s -- -b $HOME/bin
-
-if [ -d $HOME/.config/kitty ]; then
-	mv kitty.conf $HOME/.config/kitty
-else
-	mkdir $HOME/.config/kitty
-	mv kitty.conf $HOME/.config/kitty 
-fi
-
-cd $HOME/bin
-wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
-chmod +777 nvim.appimage
-
-while true; do
-read -p "Do you want to install Tidal Hifi? (yes/no) " yn
-case $yn in 
-	yes )	wget https://github.com/Mastermindzh/tidal-hifi/releases/download/5.9.0/tidal-hifi-5.9.0.AppImage
-		chmod +777 tidal-hifi-5.9.0.AppImage;
-		break;;
-	no ) echo the installation will proceed without tidal;
-		break ;;
-	* ) echo invalid response;;
-esac
-done
 
 if [ -d $HOME/.vim ]; then
 	if [ -d $HOME/.vim/plugin ]; then
@@ -56,3 +39,42 @@ else
 	mkdir $HOME/.vim/plugin
 	mv stdheader.vim $HOME/.vim/plugin
 fi
+
+if [ -d $HOME/.config/kitty ]; then
+	mv kitty.conf $HOME/.config/kitty
+else
+	mkdir $HOME/.config/kitty
+	mv kitty.conf $HOME/.config/kitty 
+fi
+
+#Installs every adition app like NVim, Tidal and Obsidian
+
+cd $HOME/bin
+wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+chmod +777 nvim.appimage
+
+while true; do
+read -p "Do you want to install Tidal Hifi? (yes/no) λ " yn
+case $yn in 
+	yes )	wget https://github.com/Mastermindzh/tidal-hifi/releases/download/5.9.0/tidal-hifi-5.9.0.AppImage -O Tidal
+		chmod +777 Obsidian;
+		break;;
+	no ) echo the installation will proceed without Tidal;
+		break ;;
+	* ) echo invalid response;;
+esac
+done
+
+while true; do
+read -p "Do you want to install Obsidian? (yes/no) λ " yn
+case $yn in 
+	yes )	wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.7.7/Obsidian-1.7.7.AppImage -O Obsidian
+		chmod +777 Obsidian;
+		break;;
+	no ) echo the installation will proceed without Obsidian;
+		break ;;
+	* ) echo invalid response;;
+esac
+done
+
+echo "===========================// λ Installation has ended λ //==========================="
