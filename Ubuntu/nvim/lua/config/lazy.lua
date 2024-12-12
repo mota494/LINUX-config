9ad2be06292d4f47a611f6036ba23815508cff6b
Mota494 <migueloureiromota@gmail.com>
2024-12-12 12:16:12 +0000

    Nvim

--- Ubuntu/nvim/lua/config/lazy.lua
+++ Ubuntu/nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "tokyonight" } },
+  install = { colorscheme = { "rose_pine" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
---
+++ Ubuntu/nvim/lua/plugins/rose_pine.lua
@@ -0,0 +1,8 @@
+return
+{ 
+	"rose-pine/neovim", 
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine-moon")
+	end
+}
